3b6f7c0d7f64286aa3d30edf75dc2521b770c997
Muhammad Mubazar Qureshi <121640226+MMubazar@users.noreply.github.com>
2024-06-27 07:55:26 +0500

    Topic: Reactive Form: add new form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import Validation from '../utils/validation';
 
 @Component({
   selector: 'app-reactive-form',
@@ -9,60 +17,122 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ReactiveFormComponent {
   title: string = 'Reactive Forms';
 
-  userForm!: FormGroup;
+  // userForm!: FormGroup;
   // phoneNumbers: any;
 
+  // -------------------------------------new code---------------------------
+  form: FormGroup = new FormGroup({
+    fullname: new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    acceptTerms: new FormControl(false),
+  });
+  submitted = false;
+
   constructor(private formBuilder: FormBuilder) {
-    this.userForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      email: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern(
-            /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/
-          ),
-        ],
-      ],
-      address: this.formBuilder.group({
-        street: ['', Validators.required],
-        city: [
+    // this.userForm = this.formBuilder.group({
+    //   name: ['', Validators.required],
+    //   email: [
+    //     '',
+    //     [
+    //       Validators.required,
+    //       Validators.pattern(
+    //         /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/
+    //       ),
+    //     ],
+    //   ],
+    //   address: this.formBuilder.group({
+    //     street: ['', Validators.required],
+    //     city: [
+    //       '',
+    //       [
+    //         Validators.required,
+    //         Validators.pattern(/^[a-zA-Z'-]+(?:\s+[a-zA-Z'-]+)*$/),
+    //       ],
+    //     ],
+    //   }),
+    //   phoneNumbers: this.formBuilder.array([
+    //     this.formBuilder.control('', [
+    //       Validators.required,
+    //       Validators.pattern(/^\d{10}$/),
+    //     ]),
+    //   ]),
+    // });
+  }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        fullname: ['', Validators.required],
+        username: [
           '',
           [
             Validators.required,
-            Validators.pattern(/^[a-zA-Z'-]+(?:\s+[a-zA-Z'-]+)*$/),
+            Validators.minLength(6),
+            Validators.maxLength(20),
           ],
         ],
-      }),
-      phoneNumbers: this.formBuilder.array([
-        this.formBuilder.control('', [
-          Validators.required,
-          Validators.pattern(/^\d{10}$/),
-        ]),
-      ]),
-    });
-  }
-
-  get phoneNumbers() {
-    return this.userForm.get('phoneNumbers') as FormArray;
-  }
-
-  addPhoneNumber() {
-    this.phoneNumbers.push(
-      this.formBuilder.control('', [
-        Validators.required,
-        Validators.pattern(/^\d{10}$/),
-      ])
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40),
+          ],
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue],
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')],
+      }
     );
   }
-  removePhoneNumber(index: number) {
-    this.phoneNumbers.removeAt(index);
+
+  // get phoneNumbers() {
+  //   return this.userForm.get('phoneNumbers') as FormArray;
+  // }
+
+  // addPhoneNumber() {
+  //   this.phoneNumbers.push(
+  //     this.formBuilder.control('', [
+  //       Validators.required,
+  //       Validators.pattern(/^\d{10}$/),
+  //     ])
+  //   );
+  // }
+  // removePhoneNumber(index: number) {
+  //   this.phoneNumbers.removeAt(index);
+  // }
+
+  // submit() {
+  //   if (this.userForm.valid) {
+  //     console.log(this.userForm.value, this.userForm);
+  //     this.userForm.reset();
+  //   }
+  // }
+
+  // -------------------------------------new code---------------------------
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
   }
 
-  submit() {
-    if (this.userForm.valid) {
-      console.log(this.userForm.value, this.userForm);
-      this.userForm.reset();
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
     }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
   }
 }
