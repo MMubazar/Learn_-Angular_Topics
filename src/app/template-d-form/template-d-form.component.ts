b3ae41caf9b84f72fac16d14163c1b2141b5114f
Muhammad Mubazar Qureshi <121640226+MMubazar@users.noreply.github.com>
2024-07-01 23:57:28 +0500

    Topic: Template-Driven Form

--- src/app/template-d-form/template-d-form.component.ts
+++ src/app/template-d-form/template-d-form.component.ts
@@ -4,45 +4,49 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template-d-form',
   templateUrl: './template-d-form.component.html',
-  styleUrl: './template-d-form.component.scss'
+  styleUrl: './template-d-form.component.scss',
 })
 export class TemplateDFormComponent {
-  title: string = "Template Driven Forms"
+  title: string = 'Template Driven Forms';
 
-  userForm: {name:string, email:string , phoneNo: string} = {
-    name: "",
-    email:"",
-    phoneNo:"",
-  }
+  userForm: { name: string; email: string; phoneNo: string } = {
+    name: '',
+    email: '',
+    phoneNo: '',
+  };
 
-  submitForm(form: NgForm){
-    if(form.valid){
-      console.log(form.value , this.userForm)
+  submitForm(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value, this.userForm);
       console.log(this.Form.value);
-      
     }
   }
 
-  validatedEmail(): boolean{
+  validatedEmail(): boolean {
     const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
-    return emailRegex.test(this.userForm.email)
+    return emailRegex.test(this.userForm.email);
   }
 
-  validatedName():boolean{
+  // validatedName():boolean{
+  //   // const nameRegex = /^[a-zA-Z'&\-_.,!@#$%^*()+={}[\]:;"'<>/\\]+(?:\s+[a-zA-Z'&\-_.,!@#$%^*()+={}[\]:;"'<>/\\]+)*$/;
+  //   const nameRegex = /^[a-zA-Z'-]+(?:\s+[a-zA-Z'-]+)*$/
+  //   ;
+  //   return nameRegex.test(this.userForm.name)
+  // }
+
+  validatedName(): boolean {
     // const nameRegex = /^[a-zA-Z'&\-_.,!@#$%^*()+={}[\]:;"'<>/\\]+(?:\s+[a-zA-Z'&\-_.,!@#$%^*()+={}[\]:;"'<>/\\]+)*$/;
-    const nameRegex = /^[a-zA-Z'-]+(?:\s+[a-zA-Z'-]+)*$/
-    ;
-    return nameRegex.test(this.userForm.name)
+    const nameRegex = /^[a-zA-Z'-]+(?:\s+[a-zA-Z'-]+)*$/;
+    return nameRegex.test(this.userForm.name);
   }
 
   @ViewChild('userInfo')
-  Form!: NgForm; 
-  reset(){
+  Form!: NgForm;
+  reset() {
     this.Form.reset({
-      name: "",
-    email:"",
-    phoneNo:"",
-    })
+      name: '',
+      email: '',
+      phoneNo: '',
+    });
   }
-
 }
