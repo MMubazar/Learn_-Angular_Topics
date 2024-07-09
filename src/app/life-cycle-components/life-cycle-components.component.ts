599bc06957d0c0781db774379081558ea31b8869
Muhammad Mubazar Qureshi <121640226+MMubazar@users.noreply.github.com>
2024-07-09 23:02:43 +0500

    updated

--- src/app/life-cycle-components/life-cycle-components.component.ts
+++ src/app/life-cycle-components/life-cycle-components.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-life-cycle-components',
   templateUrl: './life-cycle-components.component.html',
-  styleUrl: './life-cycle-components.component.scss'
+  styleUrl: './life-cycle-components.component.scss',
 })
 export class LifeCycleComponentsComponent {
+  constructor() {
+    console.log('Constructor');
+  }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges', changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
 }
