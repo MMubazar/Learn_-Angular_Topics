d6abb6d21b60266c7d6df0afacea7f35d6e94ea7
Muhammad Mubazar Qureshi <121640226+MMubazar@users.noreply.github.com>
2024-07-12 23:40:40 +0500

    Life Cycle hooks

--- src/app/life-cycle-components/life-cycle-components.component.ts
+++ src/app/life-cycle-components/life-cycle-components.component.ts
@@ -9,36 +9,51 @@ export class LifeCycleComponentsComponent {
   constructor() {
     console.log('Constructor');
   }
+  public Changes: number = 23;
 
   ngOnChanges(changes: SimpleChanges) {
     console.log('ngOnChanges', changes);
+    console.log(this.Changes);
   }
 
   ngOnInit() {
     console.log('ngOnInit');
+    console.log(this.Changes);
   }
 
   ngDoCheck() {
+    console.log(this.Changes);
+
     console.log('ngDoCheck');
   }
 
   ngAfterContentInit() {
+    console.log(this.Changes);
+
     console.log('ngAfterContentInit');
   }
 
   ngAfterContentChecked() {
+    console.log(this.Changes);
+
     console.log('ngAfterContentChecked');
   }
 
   ngAfterViewInit() {
+    console.log(this.Changes);
+
     console.log('ngAfterViewInit');
   }
 
   ngAfterViewChecked() {
+    console.log(this.Changes);
+
     console.log('ngAfterViewChecked');
   }
 
   ngOnDestroy() {
+    console.log(this.Changes);
+
     console.log('ngOnDestroy');
   }
 }
