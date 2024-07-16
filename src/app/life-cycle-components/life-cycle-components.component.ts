f2e28427fc4cabe164c816ce8e9c22720e1d6822
Muhammad Mubazar Qureshi <121640226+MMubazar@users.noreply.github.com>
2024-07-17 01:08:41 +0500

    Updated

--- src/app/life-cycle-components/life-cycle-components.component.ts
+++ src/app/life-cycle-components/life-cycle-components.component.ts
@@ -1,4 +1,4 @@
-import { Component, SimpleChanges } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-life-cycle-components',
@@ -6,55 +6,101 @@ import { Component, SimpleChanges } from '@angular/core';
   styleUrl: './life-cycle-components.component.scss',
 })
 export class LifeCycleComponentsComponent {
+  // constructor() {
+  //   console.log('Constructor');
+  // }
+  public Changes: number = 23;
+
+  // ngOnChanges(changes: SimpleChanges) {
+  //   console.log('ngOnChanges', changes);
+  //   console.log(this.Changes);
+  // }
+
+  // ngOnInit() {
+  //   console.log('ngOnInit');
+  //   console.log(this.Changes);
+  // }
+
+  // ngDoCheck() {
+  //   console.log(this.Changes);
+
+  //   console.log('ngDoCheck');
+  // }
+
+  // ngAfterContentInit() {
+  //   console.log(this.Changes);
+
+  //   console.log('ngAfterContentInit');
+  // }
+
+  // ngAfterContentChecked() {
+  //   console.log(this.Changes);
+
+  //   console.log('ngAfterContentChecked');
+  // }
+
+  // ngAfterViewInit() {
+  //   console.log(this.Changes);
+
+  //   console.log('ngAfterViewInit');
+  //   console.log('con');
+  // }
+
+  // ngAfterViewChecked() {
+  //   console.log(this.Changes);
+
+  //   console.log('ngAfterViewChecked');
+  // }
+
+  // ngOnDestroy() {
+  //   console.log(this.Changes);
+
+  //   console.log('ngOnDestroy');
+  // }
+
+  @Input() counterValue!: number;
+
   constructor() {
     console.log('Constructor');
   }
-  public Changes: number = 23;
 
   ngOnChanges(changes: SimpleChanges) {
     console.log('ngOnChanges', changes);
-    console.log(this.Changes);
   }
 
   ngOnInit() {
     console.log('ngOnInit');
-    console.log(this.Changes);
   }
 
   ngDoCheck() {
-    console.log(this.Changes);
-
     console.log('ngDoCheck');
   }
 
   ngAfterContentInit() {
-    console.log(this.Changes);
-
     console.log('ngAfterContentInit');
   }
 
   ngAfterContentChecked() {
-    console.log(this.Changes);
-
     console.log('ngAfterContentChecked');
   }
 
   ngAfterViewInit() {
-    console.log(this.Changes);
-
     console.log('ngAfterViewInit');
-    console.log('con');
   }
 
   ngAfterViewChecked() {
-    console.log(this.Changes);
-
     console.log('ngAfterViewChecked');
   }
 
   ngOnDestroy() {
-    console.log(this.Changes);
-
     console.log('ngOnDestroy');
+  }
+
+  increment() {
+    this.counterValue++;
+  }
+
+  decrement() {
+    this.counterValue--;
   }
 }
