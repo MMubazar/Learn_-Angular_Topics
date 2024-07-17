6d60d4b35c28ca9a76d18d00d57305317db8f637
Muhammad Mubazar Qureshi <121640226+MMubazar@users.noreply.github.com>
2024-07-17 23:09:14 +0500

    App component updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public initialValue = 0;
+  public formConfig = [
+    {
+      name: 'username',
+      label: 'Username',
+      type: 'text',
+      validators: [Validators.required],
+    },
+    {
+      name: 'email',
+      label: 'Email',
+      type: 'email',
+      validators: [Validators.required, Validators.email],
+    },
+    {
+      name: 'password',
+      label: 'Password',
+      type: 'password',
+      validators: [Validators.required, Validators.minLength(6)],
+    },
+  ];
   title = 'angular_all_topics';
 }
